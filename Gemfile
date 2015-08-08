b2ed390b178c1cc107c64a308f23952a57b51efa
Oleksii Lesiuk <alexeylesiuk@gmail.com>
2015-08-09 00:47:52 +0300

    Finallygit add --a!

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'rake'
 gem 'rspec'
 gem 'test-unit'
 gem 'webmock'
+gem 'activesupport'
 
 gem 'pry'
 gem 'pry-nav'
--- beacon/beacon.rb
+++ beacon/beacon.rb
@@ -13,7 +13,21 @@ case ARGV.count
         options = OptparseBeacon.parse(ARGV)
         time_from = Beacon::TimeStamp.option_to_date_time(options.from)
         time_to = Beacon::TimeStamp.option_to_date_time(options.to)
+        #minimum time is 1378395540
 
-        res = Beacon::Get.by_period( time_from, time_to )
-        puts Beacon::Record.chars_entry_to_s res
+        #I know that it possible to make nice verification somewhere in classes
+        if ( time_from.to_i < 1378395540 || time_to.to_i < 1378395540 ) then
+            puts "Please note that Beacon service started only at Thu, 05 Sep 2013 15:39:00 GMT"
+            puts "please use bigger value"
+        else
+            if time_to.nil? then 
+                time_to = DateTime.now
+            end
+            if time_from.nil? then 
+                time_from = DateTime.parse("Thu, 05 Sep 2013 15:39:00 GMT")
+            end
+            res = Beacon::Get.by_period( time_from, time_to )
+            puts Beacon::Record.chars_entry_to_s res
+        end
+    
 end
